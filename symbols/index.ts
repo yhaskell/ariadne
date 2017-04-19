80a216b54e1ffeaba9155c0461aa3df1d51b867f
Igor N. Dultsev <yhaskell@live.com>
2017-04-19 22:08:32 +0700

    remove symbols

--- symbols/index.ts
+++
@@ -1,4 +0,0 @@
-export const $tableName = Symbol("ariadne:table-name")
-export const $fields = Symbol("ariadne:fields")
-export const $dbSets = Symbol("ariadne:dbsets")
-export const $typeFor = Symbol("ariadne:type-for")
